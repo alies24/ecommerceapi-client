b9f8448cc076baf1772b5593c0c4af3db1e9f063
alies24 <ASUS@DESKTOP-6QFKKNT>
2023-03-21 21:01:55 +0300

    added alertifyjs.

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -4,6 +4,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatListModule} from '@angular/material/list';
+
 
 
 
@@ -15,7 +17,8 @@ import { CommonModule } from '@angular/common';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatListModule
   ],
 
   exports:[
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 
+
+declare var $:any;
+declare var alertify:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerceclient';
+  title = 'E-Commerce Client';
+  ngOnInit(): void {
+
+
+  }
+
 
 }
+
+
